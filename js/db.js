cbb4caf0b9bbf30292bb11e78bf07d3a31618507
anastasiia.vorobiova <anastasiiavorobiova2403@gmail.com>
2022-11-19 15:18:03 +0200

    feat/try catch for fetch

--- js/db.js
+++ js/db.js
@@ -1,9 +1,17 @@
 class Service {
   async getProducts(cb) {
-    const response = await fetch("http://localhost:3000");
-    const data = await response.json();
+    try {
+      const response = await fetch("http://localhost:3000");
+      const data = await response.json();
 
-    cb(data);
+      if (response.ok) {
+        cb(data);
+        return;
+      }
+      console.warn(response.status);
+    } catch (err) {
+      console.warn(err);
+    }
   }
 }
 
