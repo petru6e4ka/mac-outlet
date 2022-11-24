55903a2ed76a0f0ee0339ae26b6be8d7ae172502
anastasiia.vorobiova <anastasiiavorobiova2403@gmail.com>
2022-11-24 18:10:06 +0200

    refactor/commented code

--- frontend/js/pages/signup/controller.js
+++ frontend/js/pages/signup/controller.js
@@ -4,49 +4,7 @@ import template from "./template.js";
 class SignUpController {
   constructor() {
     this.view = new View(template);
-    //repository.products.getProducts(this.initialize.bind(this));
   }
-
-  // async initialize(products) {
-  //   this.filtering = filtering.get();
-  //   this.cart = new Cart();
-  //   this.products = new Products(products, this.filtering);
-  //   this.filters = new Filters(products, this.filtering);
-  //   this.controls = new Controls();
-  //   this.modal = new Modal();
-
-  //   this.controls.events.subscribe(
-  //     FILTERS.CLOSE,
-  //     this.filters.closeFilter.bind(this.filters)
-  //   );
-  //   this.controls.events.subscribe(
-  //     FILTERS.OPEN,
-  //     this.filters.openFilter.bind(this.filters)
-  //   );
-
-  //   this.products.events.subscribe(
-  //     PRODUCT.OPEN_INFO,
-  //     this.modal.openModal.bind(this.modal, productInfo)
-  //   );
-
-  //   this.filters.events.subscribe(
-  //     FILTERS.APPLY,
-  //     this.products.applyFilter.bind(this.products)
-  //   );
-  //   this.filters.events.subscribe(
-  //     FILTERS.RESET,
-  //     this.products.showAll.bind(this.products)
-  //   );
-
-  //   this.products.events.subscribe(
-  //     PRODUCT.ADD_TO_CART,
-  //     this.cart.addItem.bind(this.cart)
-  //   );
-  //   this.modal.events.subscribe(
-  //     PRODUCT.ADD_TO_CART,
-  //     this.cart.addItem.bind(this.cart)
-  //   );
-  // }
 }
 
 export default SignUpController;
