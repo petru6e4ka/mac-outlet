6ee2c194a6267f611ab8ad4c86a795d267937b2b
anastasiia.vorobiova <anastasiiavorobiova2403@gmail.com>
2022-11-25 23:39:40 +0200

    fix/ci path

--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 const connectionDB = require("./database/mongo");
 const { routes } = require("./routes");
 
@@ -11,6 +12,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(process.cwd() + "/frontend"));
+app.get("/", (_req, res) => {
+  res.sendFile(path.join(process.cwd() + "/frontend", "/index.html"));
+});
 
 routes(app);
 
--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const { getDevices } = require("../controllers/devices");
 const { createUser, loginUser } = require("../controllers/user");
 const {
@@ -7,18 +8,12 @@ const {
 } = require("../middlewares/validations");
 const { authRequired } = require("../middlewares/auth");
 
-const index = process.cwd() + "/frontend/index.html";
-
 const routes = (app) => {
   app.route("/api/devices").get(authRequired, getDevices);
   app
     .route("/api/signup")
     .post(valuesValidation, newUserValidation, createUser);
   app.route("/api/signin").post(valuesValidation, userValidation, loginUser);
-
-  app.route("/").get((_req, res) => {
-    res.sendFile(index);
-  });
 };
 
 module.exports = {
